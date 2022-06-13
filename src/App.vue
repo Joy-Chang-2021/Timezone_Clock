4d387c8409ab896a72c128d5135356e0aa3d81a1
Joy-Chang-2021 <joy50422@gmail.com>
2022-06-14 01:37:31 +0800

    feat: navbar style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app" class="container-xl">
-    <nav>
-      <div>navbar</div>
+    <nav class="navbar navbar-light my-3 rounded" style="background-color: #e3f2fd;">
+      <a class="navbar-brand mr-0" href="#">
+        <!-- TODO: ICON -->
+        <img src="/docs/4.6/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
+        <span class="navbar-brand mb-0 ml-2 h1 font-weight-bolder">Timezone</span>
+      </a>
+      <div>
+        <button type="button" class="btn btn-outline-info ml-2">AM/PM</button>
+        <button type="button" class="btn btn-outline-info ml-2">24HR</button>
+      </div>
     </nav>
     <router-view/>
   </div>
