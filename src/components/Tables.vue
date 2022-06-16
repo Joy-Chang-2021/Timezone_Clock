3860a27a3f4ce65077741037794d1953934db2b0
Joy-Chang-2021 <joy50422@gmail.com>
2022-06-17 03:25:09 +0800

    feat: draggable function

--- src/components/Tables.vue
+++ src/components/Tables.vue
@@ -1,44 +1,62 @@
 <template>
-  <table>
-    <tr>
-      <td class="left position-relative">
+  <draggable
+    class="list-group"
+    tag="ul"
+    v-model="list"
+    v-bind="dragOptions"
+    @start="drag = true"
+    @end="drag = false"
+  >
+  <transition-group type="transition">
+    <li v-for="element in list" :key="element.order" class="d-flex">
+      <div class="left position-relative">
         <div class="setting">xx</div>
         <div class="row mx-0">
           <div class="col-2">+1</div>
-          <div class="col-6">country</div>
+          <div class="col-6">{{ element.name }}</div>
           <div class="col-4">time</div>
         </div>
-      </td>
-      <td class="right">24小時時間</td>
-    </tr>
-    <tr>
-      <td class="left position-relative">
-        <div class="setting">xx</div>
-        <div class="row mx-0">
-          <div class="col-2">+1</div>
-          <div class="col-6">country</div>
-          <div class="col-4">time</div>
-        </div>
-      </td>
-      <td class="right">24小時時間</td>
-    </tr>
-    <tr>
-      <td class="left position-relative">
-        <div class="setting">xx</div>
-        <div class="row mx-0">
-          <div class="col-2">+1</div>
-          <div class="col-6">country</div>
-          <div class="col-4">time</div>
-        </div>
-      </td>
-      <td class="right">24小時時間</td>
-    </tr>
-  </table>
+      </div>
+      <div class="right">24小時時間</div>
+    </li>
+  </transition-group>
+  </draggable>
 </template>
 
 <script>
+import draggable from 'vuedraggable';
+
+const sample = [
+  "AAA",
+  "BBB",
+  "CCC"
+]
+
 export default {
   name: 'Tables',
+  components: {
+    draggable
+  },
+  data() {
+    return {
+      list: sample.map((name, index) => {
+        return { name, order: index + 1}
+      }),
+      drag: false
+    }
+  },
+  methods: {
+  },
+  computed: {
+    dragOptions() {
+      return {
+        animation: 200,
+        group: "description",
+        disabled: false,
+        ghostClass: "ghost"
+      };
+    }
+  }
 }
 </script>
 
@@ -47,5 +65,10 @@ export default {
   position: absolute;
   transform: translateX(-100%);
   padding: 0 10px;
+}
+// draggable css
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
 }
 </style>
