0fd6451848a4151b4c72343402421750a756b8b4
Joy-Chang-2021 <joy50422@gmail.com>
2022-06-20 21:56:45 +0800

    feat: table left side style

--- src/components/Tables.vue
+++ src/components/Tables.vue
@@ -8,13 +8,25 @@
     @end="drag = false"
   >
   <transition-group type="transition">
-    <li v-for="element in list" :key="element.order" class="d-flex">
+    <li v-for="element in list" :key="element.order"  class="border-bottom d-flex list-height">
       <div class="left position-relative">
-        <div class="setting">xx</div>
-        <div class="row mx-0">
+        <div class="setting d-flex position-absolute list-height">
+          <i class="fa-solid fa-circle-xmark fa-xs my-2"></i>
+          <i class="fa-solid fa-house fa-xs  my-2"></i>
+        </div>
+        <div class="row mx-0 text-align-center list-height">
           <div class="col-2">+1</div>
-          <div class="col-6">{{ element.name }}</div>
-          <div class="col-4">time</div>
+          <div class="col-6 text-left">
+            <h3 class="my-0 text-15">
+              <strong>{{ element.name }}</strong>
+              <span class="text-black-50 text-12 ml-1">XXX</span>
+            </h3>
+            <p class="my-0 text-black-50 text-12">place place place</p>
+          </div>
+          <div class="col-4 text-right">
+            <h3 class="my-0 text-black-50 text-15">time time</h3>
+            <p class="my-0 text-black-50 text-12">date date</p>
+          </div>
         </div>
       </div>
       <div class="right">24小時時間</div>
@@ -61,10 +73,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.list-height {
+  height: 4rem;
+}
+.text-15 {
+  font-size: 15px;
+}
+.text-12 {
+  font-size: 12px;
+}
 .setting {
-  position: absolute;
   transform: translateX(-100%);
-  padding: 0 10px;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: center;
+}
+.text-align-center {
+  align-items: center;
 }
 // draggable css
 .ghost {
